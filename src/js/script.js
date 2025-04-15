72ff446a98f82cee3f0ecee078beb3d54cec0906
Antonio <antoniojacintoa@gmail.com>
2025-04-15 12:04:11 -0300

    Exercicio 2

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,20 @@ let variavel3 = 10; //Variavel com valor
 console.log(variavel1)
 console.log(variavel2)
 console.log(variavel3)
+
+
+
+// EXERCICIO 2
+// Operadores comarativos retornam TRUE ou FALSE
+
+let variavel4 = 5;
+let variavel5 = 6;
+let variavel6 = '5';
+let variavel7 = '5';
+
+console.log(variavel4 >= variavel5) // Retorna falso, porque 5 não é maior ou igual a 6
+console.log(variavel4 <= variavel5) // Retorna verdadeiro, porque 5 é menor que 6
+console.log(variavel4 == variavel5) // Retorna falso, porque 5 nõ é igual a 6
+console.log(variavel4 != variavel5) // Retorna verdadeiro, porque 5 é diferente de 6
+console.log(variavel4 === variavel6) // Retorna falso, porque o === compara os valores e o tipo, sendo assim mesmo que ambas são 5, o tipo de uma é number e o tipo de outra é string
+console.log(variavel6 === variavel7) // Retorna verdadeiro, porque tanto o valor quanto o tipo das variaveis são iguais
