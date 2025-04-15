ca3ca9d79d9f86ae0a23ebd339aa3db582791b33
Antonio <antoniojacintoa@gmail.com>
2025-04-15 12:34:37 -0300

    Exercicio 3

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,32 @@ console.log(variavel4 == variavel5) // Retorna falso, porque 5 nõ é igual a 6
 console.log(variavel4 != variavel5) // Retorna verdadeiro, porque 5 é diferente de 6
 console.log(variavel4 === variavel6) // Retorna falso, porque o === compara os valores e o tipo, sendo assim mesmo que ambas são 5, o tipo de uma é number e o tipo de outra é string
 console.log(variavel6 === variavel7) // Retorna verdadeiro, porque tanto o valor quanto o tipo das variaveis são iguais
+
+
+
+
+// EXERCICIO 3
+
+function calcularIMC (peso, altura) {
+    let IMC = peso / (altura * altura)
+
+
+    switch (true) {
+        case IMC < 18.5:
+            console.log("Abaixo do peso")
+            break;
+        case IMC >= 18.5 && IMC <= 24.9:
+            console.log("Peso ideal")
+            break;
+        case IMC > 24.9:
+            console.log("Acima do peso")
+            break;
+        default:
+            break;
+    }
+}
+
+let peso = 60; // Peso em kg
+let altura = 1.70; // Altura em metros
+
+calcularIMC(peso, altura)
