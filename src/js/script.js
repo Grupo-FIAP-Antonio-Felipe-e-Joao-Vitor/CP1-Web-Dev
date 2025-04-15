21e092bcb06a5a64d3d66a8d1402290c75febca8
Antonio <antoniojacintoa@gmail.com>
2025-04-15 11:48:38 -0300

    Exercicio 1

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// EXERCICIO 1
+
+let variavel1; //Variavel undefined
+let variavel2 = ""; //Variavel com valor vazio
+let variavel3 = 10; //Variavel com valor
+
+console.log(variavel1)
+console.log(variavel2)
+console.log(variavel3)
